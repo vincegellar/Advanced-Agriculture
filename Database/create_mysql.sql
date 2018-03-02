cf24e2cb9a8c29c586d6b3bbb21262479e5cd086
Vince Gellár <gellar.vince@gmail.com>
2018-03-02 22:04:02 +0100

    Add database creation script

---
+++ Database/create_mysql.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS AdvancedAgriculture;
+CREATE DATABASE AdvancedAgriculture;
+USE AdvancedAgriculture;
+
+CREATE TABLE Plants (
+	Id INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR(255) NOT NULL,
+	SoilMoistureLowTreshold INT NOT NULL,
+	SoilMoistureHighTreshold INT NOT NULL,
+	TemperatureLowTreshold FLOAT(4,1) NOT NULL,
+	TemperatureHighTreshold FLOAT(4,1) NOT NULL,
+	HumidityLowTreshold INT NOT NULL,
+	HumidityHighTreshold INT NOT NULL,
+	LightLowTreshold INT NOT NULL,
+	LightHighTreshold INT NOT NULL,
+	PRIMARY KEY (Id)
+);
+
+CREATE TABLE Measurements (
+	MeasureTime DATETIME NOT NULL,
+	PlantId INT NOT NULL,
+	SoilMoisture INT NOT NULL,
+	Temperature FLOAT(4,1) NOT NULL,
+	Humidity INT NOT NULL,
+	Light INT NOT NULL,
+	WaterLevel INT NOT NULL,
+	PRIMARY KEY (MeasureTime, PlantId),
+	CONSTRAINT PlantId1
+	FOREIGN KEY (PlantId)
+	REFERENCES AdvancedAgriculture.Plants (Id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
+
+CREATE TABLE Settings (
+	PlantId INT NOT NULL,
+	Email VARCHAR(255) NOT NULL,
+	DarkHoursStart TIME DEFAULT NULL,
+	DarkHoursEnd TIME DEFAULT NULL,
+	SilentHoursStart TIME DEFAULT NULL,
+	SilentHoursEnd TIME DEFAULT NULL,
+	PRIMARY KEY (PlantId),
+	CONSTRAINT PlantId2
+	FOREIGN KEY (PlantId)
+	REFERENCES AdvancedAgriculture.Plants (Id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
