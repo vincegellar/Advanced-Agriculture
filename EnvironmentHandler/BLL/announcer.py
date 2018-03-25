a789bce6c926e894bbd4a866c8bdaa64c3dc59d4
Vince Gellár <gellar.vince@gmail.com>
2018-03-25 15:57:51 +0200

    Announcer on background thread

---
+++ EnvironmentHandler/BLL/announcer.py
@@ -0,0 +1,31 @@
+from time import sleep
+from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, gethostbyname, gethostname
+import threading
+import configparser
+from os import path
+
+PORT = 50000
+IDENTIFIER = 'advanced_agriculture:'
+
+config = configparser.RawConfigParser()
+config.read(path.join(path.dirname(__file__), '../EnvironmentHandler.cfg'))
+announce_interval = config.get('Announce', 'interval')
+
+
+class Announcer(object):
+
+    def __init__(self):
+        self.interval = int(announce_interval)
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(('', 0))
+        self.socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        self.ip = gethostbyname(gethostname())
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
+        while True:
+            data = IDENTIFIER + self.ip
+            self.socket.sendto(data.encode('utf-8'), ('<broadcast>', PORT))
+            sleep(self.interval)
