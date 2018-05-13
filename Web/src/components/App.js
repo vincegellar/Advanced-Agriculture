f5702a7ae91d5904fea9437ec97a5eb9b122124b
Viktor Bán <viktor.ban@gmail.com>
2018-05-13 14:08:45 +0200

    Clean create-react-app bloat

--- Web/src/components/App.js
+++ Web/src/components/App.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
 import './App.css';
-import Icon from './Icon';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <Icon name="water"/>
-        </p>
-      </div>
+        <div className="App">
+        </div>
     );
   }
 }
