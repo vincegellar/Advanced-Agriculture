ce48308764e1dd7a2cc56aa4010168db2c737311
Vince Gellár <gellar.vince@gmail.com>
2018-05-15 19:08:29 +0200

    Remove unnecessary field from JSON response

--- EnvironmentHandler/EnvironmentHandler.py
+++ EnvironmentHandler/EnvironmentHandler.py
@@ -87,11 +87,11 @@ def get_plants():
 def get_current():
     response = {}
     for plant_id, measurement in last_measurement.items():
-        response[plant_id]['current_measurement'] = {'water_level': measurement.water,
-                                                     'temp': measurement.temperature,
-                                                     'humidity': measurement.humidity,
-                                                     'light': measurement.light,
-                                                     'soil_moisture': measurement.moisture}
+        response[plant_id] = {'water_level': measurement.water,
+                              'temp': measurement.temperature,
+                              'humidity': measurement.humidity,
+                              'light': measurement.light,
+                              'soil_moisture': measurement.moisture}
     return jsonify(response)
 
 
